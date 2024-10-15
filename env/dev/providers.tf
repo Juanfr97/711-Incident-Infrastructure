c673c4388f351545e6473570948772c691cc0427
Juan Frausto <juanfr97@hotmail.com>
2024-10-14 19:24:06 -0600

    Dev Deployment

--- env/dev/providers.tf
+++ env/dev/providers.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name = "example-resources"
+    storage_account_name = "juanfraccount"
+    container_name = "711-106945-tfstate"
+    key = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
